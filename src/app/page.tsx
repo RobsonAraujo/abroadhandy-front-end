e072301dde00e7d40b868126ba746d859a312b96
robshow <robsonaraujocarmo@hotmail.com>
2025-09-06 23:18:05 -0400

    feat: lowercase test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Hero from "./components/Hero/Hero";
+import Hero from "./components/hero/Hero";
 import HowItWorks from "./components/how-it-works/HowItWorks";
 import CTASimple from "./components/cta-simple/CTASimple";
 import CTA from "./components/cta/CTA";
